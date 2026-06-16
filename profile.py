f32a776b098b3c4b8d356f3fd36f67201f51827e
Alex Mendoza <alex@Alexs-MacBook-Pro.local>
2026-06-16 13:44:16 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,15 @@
+
+full_name= "Alexander Mendoza"
+nickame= "alex"
+age= 16
+
+used_python_before = False
+hobbies = ["building kits", "drawing", "crafts"] 
+
+favorites = { 
+ "anime": "yugioh duel monsters", 
+ "food": "aguachile", 
+ "artist": "ken carson" 
+} 
+
+print(full_name, age, hobbies)
